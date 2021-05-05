3415ff5208efc8e6be0c8b2b9c08c674f437a99c
Hallysson <hallyssonrdev@gmail.com>
2021-05-05 12:53:49 -0300

    table

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
 	<title></title>
 </head>
 <body>
-	
+	<section class="hls-table-games">
+		<div>
+			<div class="table-title-games">
+				<h3></h3>
+			</div>
+			<div class="table-img-games">
+				
+			</div>
+		</div>
+	</section>
 </body>
 </html>

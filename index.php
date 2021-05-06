237f91784d77a2f455f1914a98f7a2b02273fce2
Hallysson <hallyssonrdev@gmail.com>
2021-05-06 18:45:50 -0300

    content

--- index.php
+++ index.php
@@ -1,18 +1,87 @@
+<?php 
+$json_content = file_get_contents("https://www.freetogame.com/api/games");
+$result = json_decode($json_content, true);
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 <body>
-	<section class="hls-table-games">
-		<div>
-			<div class="table-title-games">
-				<h3></h3>
-			</div>
-			<div class="table-img-games">
-				
-			</div>
+	<header>
+		ToFri
+	</header>
+	
+	<div class="row">
+		<div class="column">
+			<?php 
+			for ($i=0; $i <= 6 ; $i++) { 
+				echo "
+				<div class='card'>
+				<div>
+					<img src=" . $result[$i]['thumbnail'] . ">
+					<h4>" . $result[$i]['title'] . " </h4>	
+				</div>
+				</div>
+				";
+			}	
+			?>
 		</div>
-	</section>
+
+		<div class="column">
+			<?php 
+			for ($i=7; $i <= 13 ; $i++) { 
+				echo "
+				<div class='card'>
+				<div>
+					<img src=" . $result[$i]['thumbnail'] . ">
+					<h4>" . $result[$i]['title'] . " </h4>	
+				</div>
+				</div>
+				";
+			}	
+			?>
+		</div>
+
+		<div class="column">
+			<?php 
+			for ($i=14; $i <= 20 ; $i++) { 
+				echo "
+				<div class='card'>
+				<div>
+					<img src=" . $result[$i]['thumbnail'] . ">
+					<h4>" . $result[$i]['title'] . " </h4>	
+				</div>
+				</div>
+				";
+			}	
+			?>
+		</div>
+
+		<div class="column">
+			<?php 
+			for ($i=21; $i <= 20 ; $i++) { 
+				echo "
+				<div class='card'>
+				<div>
+					<img src=" . $result[$i]['thumbnail'] . ">
+					<h4>" . $result[$i]['title'] . " </h4>	
+				</div>
+				</div>
+				";
+			}	
+			?>
+		</div>
+
+
+
+	</div>
+	<div class="container body-content">
+		<footer>
+			Made by HLS
+		</footer>
+	</div>
 </body>
 </html>

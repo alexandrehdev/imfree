f353606d8021d9e66cdc73fc5fc5b347ea1dfb26
alexandre <hallyssonrdev@gmail.com>
2022-08-15 18:16:16 -0300

    Update View.php

--- src/Utils/View.php
+++ src/Utils/View.php
@@ -8,41 +8,41 @@ class View{
 
     
 	private static function getContentView(string $view) :string{
-		$file = __DIR__ . "/../../resources/pages/{$view}.html";
-		return file_exists($file) ? file_get_contents($file) : '';
-    }
+	   $file = __DIR__ . "/../../resources/pages/{$view}.html";
+	   return file_exists($file) ? file_get_contents($file) : '';
+        }
 
-    public static function getPathToElements(){
-        return self::pathToElements;
-    }
+    	public static function getPathToElements(){
+           return self::pathToElements;
+        }
 
 	public static function getNameElements() :array{
-		$elements = scandir(self::getPathToElements());
-		$elements = array_slice($elements,2);
+	   $elements = scandir(self::getPathToElements());
+	   $elements = array_slice($elements,2);
 
-		return $elements;
+	   return $elements;
 	}
 
 	public static function getElements() :array{
-		$files = [];
-		$elements = self::getNameElements();
-		rsort($elements);
+	  $files = [];
+	  $elements = self::getNameElements();
+	  rsort($elements);
 
-		foreach($elements as $key => $element){
-			$files[$key] = file_get_contents(self::getPathToElements() . $element);
-		}
+	  foreach($elements as $key => $element){
+	    $files[$key] = file_get_contents(self::getPathToElements() . $element);
+	  }
 
-		return $files;
+	  return $files;
 	}
 
     public static function render(string $view, array $vars) :mixed{
-        $contentView = self::getContentView($view);
-        $keys = array_keys($vars);
-		$keys = array_map(function($item){
-			return '{{'.$item.'}}';
-		},$keys);
+         $contentView = self::getContentView($view);
+         $keys = array_keys($vars);
+	 $keys = array_map(function($item){
+		return '{{'.$item.'}}';
+	 },$keys);
 
-		return str_replace($keys, array_values($vars), $contentView);
+	 return str_replace($keys, array_values($vars), $contentView);
     }
     
 

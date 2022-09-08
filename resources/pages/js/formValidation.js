96167a03a4dd53acc74a960934e68ee149bceebb
alexandrehdev <hallyssonrdev@gmail.com>
2022-09-07 22:31:37 -0300

    adding comment

--- resources/pages/js/formValidation.js
+++ resources/pages/js/formValidation.js
@@ -6,7 +6,8 @@ let form = document.forms["formRegister"];
 
 registerBtn.onclick = function(e){
     e.preventDefault();
-
+     // validar campo vazio 
+     // validar se campo senha e confirmar senha são iguais
     keys.forEach(function(key){
         if(form[key].value.length === 0){
             console.log(key + " está vazio");
